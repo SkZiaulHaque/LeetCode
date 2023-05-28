184e102a8e7c14a5c3a77d323edcdff52be2a08f
SkZiaulHaque <110910956+SkZiaulHaque@users.noreply.github.com>
2023-05-28 12:42:40 +0530

    Add files via upload

--- leetCode/src/main/java/org/example/GreatestCommonDivisorOfStringsAttempt2.java
+++ leetCode/src/main/java/org/example/GreatestCommonDivisorOfStringsAttempt2.java
@@ -1,42 +1,52 @@
 package org.example;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class GreatestCommonDivisorOfStringsAttempt2 {
+
+
+
+    public static void gcdOfStrings(String str1, String str2) {
+        System.out.println(str1 + str2);
+        return;
+    }
     public static void main (String[] args) {
+        for (int i=0; i<5; i++) {
 
-        for (int i=0; i<5; i++ )
-        {
-            System.out.println("Please enter your 1st String");
+        System.out.println("Please enter your 1st String");
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            String str1 = sc.next();
+        String str1 = sc.next();
+        System.out.println("Please enter your 2nd String");
+        String str2 = sc.next();
 
-            System.out.println("Please enter your 2nd String");
+        gcdOfStrings("your input is " + str1 + " & ", str2);
 
-            String str2 = sc.next();
+        System.out.println("**************************************************");
 
-            System.out.println("Two Strings are :: " + str1 + " & " + str2);
 
-            if ((str1 + str2).equals(str2 + str1)) {
-                System.out.println("GCD is possible");
-            } else {
-                System.out.println("GCD is not possible");
-            }
-            if (str1.equals(str2)) {
-                System.out.println("GCD is : " + str1);
-            } else if (str1.length() > str2.length()) {
-                System.out.println("GCD is : " + str1.substring(str2.length()) ) ;
-                // pls see the line number 32... substring syntax is getting wrong.. I am putting wrong I think
-            } else if (str2.length() > str1.length()) {
-                System.out.println("GCD is : " + str2.substring(str1.length()) );
-            }
+        if ((str1 + str2).equals(str2 + str1)) {
+            System.out.println("GCD is possible");
+            System.out.println("**************************************************");
+        } else {
+            System.out.println("GCD is not possible");
+            System.out.println("**************************************************");
         }
-
-
+        if (str1.equals(str2)) {
+            System.out.println("GCD is : " + str1);
+            System.out.println("**************************************************");
+        }
+        if (str1.length() > str2.length()) {
+            gcdOfStrings("GCD is :: " + str1.substring(str2.length()), "");
+            System.out.println("**************************************************");
+        } else {
+            gcdOfStrings("GCD is :: " + str2.substring(str1.length()), "");
+            System.out.println("**************************************************");
+        }
+            System.out.println("Program will for next input.....");
+        System.out.println("**************************************************");
+    }
 
 
     }
